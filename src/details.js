5354efa63e80dd4981b72502a849c298bca79b03
1334 <1334@shadowproject.net>
2018-10-29 13:11:52 +0100

    implement carousel for animal images

--- src/details.js
+++ src/details.js
@@ -2,6 +2,8 @@ import React from 'react';
 import pf from 'petfinder-client';
 import { navigate } from '@reach/router';
 
+import Carousel from './carousel';
+
 const petfinder = pf({
   key: process.env.API_KEY,
   secret: process.env.API_SECRET
@@ -36,9 +38,10 @@ class Details extends React.Component {
   render() {
     if (this.state.loading) return <h1>loading ...</h1>;
 
-    const { name, animal, breed, location, description } = this.state;
+    const { name, animal, breed, location, description, media } = this.state;
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>
