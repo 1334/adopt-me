7f68c310e08e452b7bb7add1229d5ff6b7642455
1334 <1334@shadowproject.net>
2018-10-29 10:49:06 +0100

    add Reach Router to the app

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router } from '@reach/router';
 
 import Results from './results';
+import Details from './details';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Results />
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
